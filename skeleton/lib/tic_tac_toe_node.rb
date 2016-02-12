c7fe0ee05d321e73184406428774760eb262264b
App Academy Student <appacademy@Apps-Mac-mini-85.local>
2016-02-12 17:25:03 -0500

    Create #losing_node? method

--- skeleton/lib/tic_tac_toe_node.rb
+++ skeleton/lib/tic_tac_toe_node.rb
@@ -9,6 +9,22 @@ class TicTacToeNode
   end
 
   def losing_node?(evaluator)
+    if board.over?
+      return false if board.winner == evaluator || board.tied?
+      return true
+    end
+    child_array = children
+    if evaluator != next_mover_mark
+      return true if child_array.any? { |child| child.losing_node?(evaluator) }
+    end
+    return false unless child_array.all? { |child| child.losing_node?(evaluator) }
+    # children.each do |child|
+    #   losing = child.losing_node?(evaluator)
+    #   return true if evaluator != next_mover_mark && losing
+    #   return false unless losing
+    # end
+
+    true
   end
 
   def winning_node?(evaluator)
@@ -17,5 +33,22 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    child_array = []
+
+    board.rows.each_with_index do |row, i|
+      row.each_index do |j|
+        if board.empty?([i, j])
+          child_board = board.dup
+          child_board[[i, j]] = next_mover_mark
+          child_prev_move = [i, j]
+          child_next_mark = (next_mover_mark == :x ? :o : :x)
+          child = TicTacToeNode.new(child_board, child_next_mark, child_prev_move)
+          child_array << child
+        end
+      end
+    end
+
+    child_array
   end
+
 end
